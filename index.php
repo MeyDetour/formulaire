483ba4d2f47bf1602f2089f8c248582fd4aa8e75
mey <meydetour@gmail.com>
2023-11-23 08:55:01 +0100

    s

--- index.php
+++ index.php
@@ -11,21 +11,11 @@
 </head>
 <body>
 <?php
-require_once 'secret.php'
+require_once 'secret.php';
+
+echo $content;
 ?>
-<div class="container d-flex flex-column p-5">
-    <p>Hello ! I dare you to find my secret ! </p>
-<form action="" method="post" class="d-flex flex-column w-20 fs-2 ">
 
 
-    <input type="text" name="prenominput" id="prenominput" value="" class="p-2" placeholder="Username..">
-    <input type="password" name="mdpinput" id="mdpinput" value="" class="p-2" placeholder="mot de passe..">
-    <button class="btn btn-primary" type="submit" id="submitFOrm">OK</button>
-
-</form>
-
-<span class="text-primary"><?=$secretAfficher?></span>
-<span class="text-danger fs-4"><?=$messageErreur?></span>
-</div>
 </body>
 </html>
--- secret.php
+++ secret.php
@@ -1,13 +1,12 @@
 
 <?php
-$prenomBon =  "mey";
-$prenominput = "";
+
 $mdpinput = "";
-$prenominput = $_POST['prenominput'];
-$mdpinput = $_POST['mdpinput'];
+
 
 $secretAfficher = "Decouvre mon secret";
 $messageErreur = "";
+
 $listeSecret = [
     [
         'prenom' => 'pierre',
@@ -35,20 +34,46 @@ $listeSecret = [
         'secret' => "J'aime rl",
     ]
 ];
-foreach ($listeSecret as $secret ){
-echo $secret['prenom'];
-    if ($prenominput != $secret['prenom'] ){
+if ($_POST['prenominput']!= "" && $_POST['mdpinput']!= "" ){
+foreach ($listeSecret as $secret ) {
+
+
+
+    if ($_POST['prenominput'] != $secret['prenom']) {
+
         $messageErreur = "MAUVAIS PSEUDO BAHAHA";
-    }
-    elseif ($mdpinput != $secret['mdp'] ){
+    } elseif ($_POST['mdpinput'] != $secret['mdp']) {
+
         $messageErreur = "MAUVAIS MDP BAHAHA";
-    }
-    elseif ($prenominput == $secret['prenom']  && $mdpinput== $secret['mdp']){
-        $secretAfficher = "<h1> ".$secret['prenom'] ." : </h1> Here's my secret  ,". $secret['secret'];
-    }
-    else{
+    } elseif ($_POST['prenominput'] == $secret['prenom'] && $_POST['mdpinput'] == $secret['mdp']) {
+        $messageErreur = "Bien joué";
+        $secretAfficher = "<h1> " . $secret['prenom'] . " : </h1> Here's my secret  ," . $secret['secret'];
+
+    } else {
+        echo 'else' ;
         $messageErreur = "MAUVAIS LOGIN BAHAHA";
     }
-}
+}};
+
+
+$formulaire = '<form action="" method="post" class="d-flex flex-column w-20 fs-2 ">
+    <input type="text" name="prenominput" id="prenominput" value="" class="p-2" placeholder="Username..">
+    <input type="password" name="mdpinput" id="mdpinput" value="" class="p-2" placeholder="mot de passe..">
+    <button class="btn btn-primary" type="submit" id="submitFOrm">OK</button>
+
+</form>';
+
+$secretdevoile = '<span class="text-primary">'.$secretAfficher.'</span>
+<span class="text-danger fs-4">'.$messageErreur.'</span>';
+
+
+
+$content = '
+<div class="container d-flex flex-column p-5">
+    <p>Hello ! I dare you to find my secret ! </p>'.
+    $formulaire.
+    $secretdevoile.'
+</div>';
+
 
 ?>
